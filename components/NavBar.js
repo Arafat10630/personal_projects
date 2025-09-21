08ed7b6528f2757869327d1cba0828e27c52d528
ajmozumder_seneca <188126722+ajmozumder_seneca@users.noreply.github.com>
2025-09-21 12:27:58 -0400

    first commit

---
+++ components/NavBar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Link from 'next/link'
+
+function NavBar(){
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link href="/" className="navbar-brand">
+                DEVAT
+            </Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <Link className="nav-link" href="/cart"><i className="fa-solid fa-cart-shopping"></i> Cart</Link>                    
+                    </li>
+                    
+                    <li className="nav-item active">
+                        <Link className="nav-link" href="/signin"><i className="fa-solid fa-user"></i> Sign in</Link>                    
+                    </li>
+
+                    {/* <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        User Profile
+                        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a className="dropdown-item" href="#">Profile</a>
+                            <a className="dropdown-item" href="#">Logout</a>
+                        </div>
+                    </li> */}
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,24 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+class MyDocument extends Document{
+  render(){
+    return(
+      <html lang="en">
+        <Head>
+          <meta name="description" content="Dev AT E-commerce website with Next.js"/>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"></link>
+          <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+          <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-pV7...yourhash..." crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
 }
+
+export default MyDocument
